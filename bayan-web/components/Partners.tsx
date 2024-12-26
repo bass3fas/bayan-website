59efb40fa8335c100054553918b515af3dbcb3d9
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-26 09:03:15 +0400

    the right animation behaviour, with fade and stop on hover

--- bayan-web/components/Partners.tsx
+++ bayan-web/components/Partners.tsx
@@ -22,6 +22,24 @@ const partnersData: PartnerProps[] = [
     brief: "Partner Three offers a wide range of medical devices and services.",
     logo: "/assets/logos/partnerthree.png",
   },
+  {
+    name: "Partner four",
+    link: "https://www.partnerone.com",
+    brief: "Partner One is a leading provider of medical equipment and supplies.",
+    logo: "/assets/logos/partnerone.png",
+  },
+  {
+    name: "Partner five",
+    link: "https://www.partnertwo.com",
+    brief: "Partner Two specializes in innovative healthcare solutions.",
+    logo: "/assets/logos/partnertwo.png",
+  },
+  {
+    name: "Partner siz",
+    link: "https://www.partnerthree.com",
+    brief: "Partner Three offers a wide range of medical devices and services.",
+    logo: "/assets/logos/partnerthree.png",
+  },
 ];
 
 export default function Partners() {
@@ -29,21 +47,22 @@ export default function Partners() {
 
   useEffect(() => {
     setPartners(partnersData);
-    console.log("Partners data:", partnersData);
   }, []);
 
   return (
     <div id="partners" className="cards-container">
-      <div className="flex flex-row items-center w-full h-screen bg-gray-100">
-        {partners.map((partner) => (
-          <PartnerCard
-            key={partner.name}
-            name={partner.name}
-            link={partner.link}
-            brief={partner.brief}
-            logo={partner.logo}
-          />
-        ))}
+      <div className="relative w-full h-screen overflow-hidden bg-gray-100">
+        <div className="absolute flex space-x-10 animate-slide-loop">
+          {partners.map((partner) => (
+            <PartnerCard
+              key={partner.name}
+              name={partner.name}
+              link={partner.link}
+              brief={partner.brief}
+              logo={partner.logo}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
