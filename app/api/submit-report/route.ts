ea63d85cdd5c4c03ec26ab101cfee63c11f02c21
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2025-01-31 16:43:49 +0400

    submit error fixed

--- app/api/submit-report/route.ts
+++ app/api/submit-report/route.ts
@@ -1,18 +1,10 @@
-// app/api/submit-report/route.js
 import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
 import { sendEmail } from '@/utils/sendEmail';
 
-export async function POST(req) {
-    const filePath = path.join(process.cwd(), 'constants', 'reports.json');
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const reports = JSON.parse(fileContents);
-
+export async function POST(req: Request) {
     const body = await req.json();
 
     const newReport = {
-        id: reports.length + 1,
         name: body.name,
         email: body.email,
         message: body.message,
@@ -21,16 +13,16 @@ export async function POST(req) {
         timestamp: new Date().toISOString(),
     };
 
-    reports.push(newReport);
-
-    //fs.writeFileSync(filePath, JSON.stringify(reports, null, 2));
-
-    // Send email
     const emailSubject = 'New Violation Report Submitted';
     const emailText = `A new violation report has been submitted:\n\n${JSON.stringify(newReport, null, 2)}`;
     const emailHtml = `<p>A new violation report has been submitted:</p><pre>${JSON.stringify(newReport, null, 2)}</pre>`;
 
-    await sendEmail(process.env.EMAIL_RECEIVER, emailSubject, emailText, emailHtml);
+    try {
+        await sendEmail(process.env.EMAIL_RECEIVER, emailSubject, emailText, emailHtml);
 
-    return NextResponse.json({ message: 'Report submitted successfully' });
+        return NextResponse.json({ message: 'Report submitted successfully' });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return NextResponse.json({ error: 'Failed to submit report' }, { status: 500 });
+    }
 }
